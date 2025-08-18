a240757429d074984704faa250418776b01b95c6
Ahmed-Abdo1 <a.01157571094@gmail.com>
2025-08-18 16:19:29 +0300

    chaning

--- src/Helper/config.py
+++ src/Helper/config.py
@@ -1,9 +1,9 @@
 from pydantic_settings import BaseSettings , SettingsConfigDict
 
 class Setting(BaseSettings):
-    APP_Name:str
-    App_version:str
-    OPENAI_API_KEY:str
+    APP_Name: str
+    App_version: str
+    OPENAI_API_KEY: str
 
     class congfig:
         env_file='.env'
